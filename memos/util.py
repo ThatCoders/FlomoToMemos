53a51c118388551fe56a8d0553b0118c0459f9c8
钟意 <nepenthic@163.com>
2023-08-19 13:39:39 +0800

    Update util.py

--- memos/util.py
+++ memos/util.py
@@ -11,12 +11,40 @@ def timeToUnix(zh):
     return ts_str
 
 
-def getType(filename):
-    file_type, _ = mimetypes.guess_type(filename)
-    if file_type:
-        content_type, _ = file_type.split('/')
+def getType(file_ext):
+    file_type = ''
+    if file_ext == ".jpg" or file_ext == ".jpeg":
+        file_type = 'image/jpeg'
+    elif file_ext == ".png":
+        file_type = 'image/png'
+    elif file_ext == ".mp3" or file_ext == ".wav":
+        file_type = 'audio/mpeg'
+    elif file_ext == ".doc" or file_ext == ".docx":
+        file_type = 'application/msword'
+    elif file_ext == ".pdf":
+        file_type = 'application/pdf'
+    elif file_ext == ".txt":
+        file_type = 'text/plain'
+    elif file_ext == ".xls" or file_ext == ".xlsx":
+        file_type = 'application/vnd.ms-excel'
+    elif file_ext == ".ppt" or file_ext == ".pptx":
+        file_type = 'application/vnd.ms-powerpoint'
+    elif file_ext == ".xml":
+        file_type = 'text/xml'
+    elif file_ext == ".zip":
+        file_type = 'application/zip'
+    elif file_ext == ".apk":
+        file_type = 'application/vnd.android.package-archive'
+    elif file_ext == ".exe":
+        file_type = 'application/octet-stream'
+    elif file_ext == ".js":
+        file_type = 'text/javascript'
+    elif file_ext == ".css":
+        file_type = 'text/css'
+    elif file_ext == ".html":
+        file_type = 'text/html'
     else:
-        content_type = 'application/octet-stream'
-    return content_type
+        file_type = 'application/octet-stream'
+    return file_type
 
 
